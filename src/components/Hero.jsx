4559216055356979bc36e402af1b4b15754f5874
Harikrishnan <harikrishnanb24@gmail.com>
2023-09-08 03:16:55 +0530

    fourth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import { logo } from '../assets'
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-    <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-      <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+    <nav className='flex justify-between items-center w-full mb-10 pt-6'>
+      <div className='w-28 object-contain' />
 
       <button
         type='button'
